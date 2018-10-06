451c559dd6745914a056d347c76a030d284c2696
Kajornsak Peerapathananont <kajornsak.peerapathananont@agoda.com>
2018-10-06 17:37:54 +0700

    Initial Commit

---
+++ testCMMotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testCMMotion
+//
+//  Created by Peerapathananont, Kajornsak (Agoda) on 6/10/2561 BE.
+//  Copyright © 2561 Peerapathananont, Kajornsak (Agoda). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ testCMMotionTests/testCMMotionTests.swift
@@ -0,0 +1,34 @@
+//
+//  testCMMotionTests.swift
+//  testCMMotionTests
+//
+//  Created by Peerapathananont, Kajornsak (Agoda) on 6/10/2561 BE.
+//  Copyright © 2561 Peerapathananont, Kajornsak (Agoda). All rights reserved.
+//
+
+import XCTest
+@testable import testCMMotion
+
+class testCMMotionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
